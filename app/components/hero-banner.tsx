01d597eb86f887c9d680a68982dc77446b7830d2
personal <hasinmahmood01@gmail.com>
2024-01-30 19:02:43 +0000

    feat: content for actions we take

--- app/components/hero-banner.tsx
+++ app/components/hero-banner.tsx
@@ -1,14 +1,22 @@
+import Image from "next/image";
+
 const HeroBanner = () => {
   return (
-    <div className="px-12 mt-24 flex flex-col gap-4">
-      <h1 className="font-black text-3xl">
-        Taking action today for a brighter, more promising tomorrow.
-      </h1>
-      <h2 className=" text-lg">
-        Our <span className="font-semibold">objective</span> is to{" "}
-        <span className="font-semibold">serve mankind</span> and{" "}
-        <span className="font-semibold">protect humanity</span>.
-      </h2>
+    <div className="relative w-full flex flex-col min-h-72 lg:bg-none">
+      {/* Background image for mobile */}
+      <div className="absolute inset-0 w-full h-full bg-no-repeat bg-cover bg-center lg:hidden bg-[url('/mobile-hero-bg.svg')]"></div>
+
+      {/* Text container with padding */}
+      <div className="z-10 flex flex-col gap-4 absolute h-full items-start justify-end px-12 pb-2">
+        <h1 className="font-black text-3xl">
+          Taking action today for a brighter, more promising tomorrow.
+        </h1>
+        <h2 className="text-lg">
+          Our <span className="font-semibold">objective</span> is to{" "}
+          <span className="font-semibold">serve mankind</span> and{" "}
+          <span className="font-semibold">protect humanity</span>.
+        </h2>
+      </div>
     </div>
   );
 };
---
+++ public/actions.ts
@@ -0,0 +1,13 @@
+type ActionsData = {
+  action: string;
+  info: string;
+  img: string;
+};
+
+export const actionsArr: ActionsData[] = [
+  {
+    action: "Nourish the Future",
+    info: "Delivering meals to children and families, ensuring people don’t go hungry",
+    img: "/mobile-hero-bg.svg",
+  },
+];
