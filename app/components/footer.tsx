56c11f84262811e1ab73d436690959e32e54aac9
personal <hasinmahmood01@gmail.com>
2024-01-31 23:53:52 +0000

    feat: responsiveness upto laptop

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="w-full pt-8 border-t">
-      <div className="px-12">
+      <div className="px-12 lg:px-28 xl:px-44">
         <h2 className="font-black">
           PROTECT <span className="text-sky-500">HUMANITY</span>
         </h2>
--- app/components/missions.tsx
+++ app/components/missions.tsx
@@ -1,8 +1,10 @@
 const Mission = () => {
   return (
-    <div className="flex px-12 flex-col w-full items-start">
-      <h2 className="font-bold text-2xl">Our mission</h2>
-      <p>
+    <div className="flex px-12 flex-col gap-8 w-full items-start md:items-center lg:px-28 xl:px-44">
+      <h2 className="font-bold text-2xl sm:text-3xl xl:text-4xl">
+        Our mission
+      </h2>
+      <p className="md:text-center md:max-w-[40rem] lg:max-w-[44rem] xl:text-lg">
         We&apos;re more than a charity; we&apos;re a beacon of hope. United in
         compassion, our objective stands clear and unwavering: to serve mankind
         and protect the fabric of humanity.
