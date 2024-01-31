ce6f8c77c6e5aa26acef88ec78fd34f247422fa2
personal <hasinmahmood01@gmail.com>
2024-01-31 23:52:13 +0000

    fix: remove center child components

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import HeroBanner from "./components/hero-banner";
 import Nav from "./components/nav";
-import Actions from "./components/actions";
-import Mission from "./components/missions";
-import Footer from "./components/footer";
 import ContentCont from "./components/content-container";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-4 py-16">
+    <main className="flex min-h-screen flex-col gap-4 py-16 ">
       <Nav />
       <ContentCont />
     </main>
