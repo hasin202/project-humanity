25c3948ee3116a87a762772cd6b602a434f014c7
personal <hasinmahmood01@gmail.com>
2024-01-31 16:12:53 +0000

    chore: use next image for hero bg image

--- app/components/hero-banner.tsx
+++ app/components/hero-banner.tsx
@@ -3,10 +3,18 @@ import Image from "next/image";
 const HeroBanner = () => {
   return (
     <div className="relative w-full flex flex-col min-h-72 lg:bg-none">
-      {/* Background image for mobile */}
-      <div className="absolute inset-0 w-full h-full bg-no-repeat bg-cover bg-center lg:hidden bg-[url('/mobile-hero-bg.svg')]"></div>
+      {/* Background image for mobile using Next.js Image component */}
+      <div className="absolute inset-0 w-full h-full lg:hidden">
+        <Image
+          src="/mobile-hero-bg.svg"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+          alt="Background"
+          priority
+        />
+      </div>
 
-      {/* Text container with padding */}
       <div className="z-10 flex flex-col gap-4 absolute h-full items-start justify-end px-12 pb-2">
         <h1 className="font-black text-3xl">
           Taking action today for a brighter, more promising tomorrow.
