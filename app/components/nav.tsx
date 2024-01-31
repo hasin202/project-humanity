4705ea1db60c3c45f47489e932c2f9b2c1adccf3
personal <hasinmahmood01@gmail.com>
2024-01-31 14:57:37 +0000

    fix: update name from projec to protect

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,7 +1,7 @@
 const Nav = () => {
   return (
     <div className="w-full flex items-start px-12">
-      <h1 className="font-black">PROJECT HUMANITY</h1>
+      <h1 className="font-black">PROTECT HUMANITY</h1>
     </div>
   );
 };
