3a94cce50d91d30e31853a182cbc80497e6f47ca
personal <hasinmahmood01@gmail.com>
2024-02-02 18:49:47 +0000

    feat: remove hero banner and add padding for 2xl screens

--- app/components/missions.tsx
+++ app/components/missions.tsx
@@ -1,11 +1,11 @@
 const Mission = () => {
   return (
-    <div className="flex px-12 flex-col gap-8 w-full items-start md:items-center lg:px-28 xl:px-44">
+    <div className="flex px-12 flex-col gap-8 w-full items-start md:items-center lg:px-28 xl:px-44 2xl:px-80">
       <h2 className="font-bold text-2xl sm:text-3xl xl:text-4xl">
         Our mission
       </h2>
       <p className="md:text-center md:max-w-[40rem] lg:max-w-[44rem] xl:text-lg">
-        We&apos;re more than a charity; we&apos;re a beacon of hope. United in
+        We&apos;re more than a charity. We&apos;re a beacon of hope. United in
         compassion, our objective stands clear and unwavering: to serve mankind
         and protect the fabric of humanity.
       </p>
